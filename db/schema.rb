2722237e496c3b2e96c7419acffc103103e7883c
Santosh <santosh.poudyal@gmail.com>
2018-10-15 11:31:00 +1100

    updated relationships among the tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_235123) do
+ActiveRecord::Schema.define(version: 2018_10_14_235450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "group_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_users_on_group_id"
+    t.index ["user_id"], name: "index_group_users_on_user_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -33,4 +42,6 @@ ActiveRecord::Schema.define(version: 2018_10_14_235123) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
 end
